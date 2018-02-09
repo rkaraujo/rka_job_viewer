f9e3f7e19fc9baeaf658d64d5ecd8c93ed235fdd
rkaraujo <rkaraujo@gmail.com>
2018-02-09 19:37:17 -0200

    Filtering by title

--- import-jobs.py
+++ import-jobs.py
@@ -3,7 +3,7 @@ import sqlite3
 
 conn = sqlite3.connect("/Development/workspace/rka_job_viewer/db.sqlite3")
 
-json_files = ['file_scraped_08-02-18.json']
+json_files = ['file_scraped_09-02-18.json']
 
 for json_file in json_files:
     df = pd.read_json('/Development/workspace/rka-job-scraper/' + json_file, encoding='utf-8')
@@ -20,9 +20,14 @@ for json_file in json_files:
     df = df[df['description'].str.contains('Java') | df['title'].str.contains('Java')]
     print df.shape
 
-    EXCLUDE_STR = ['alphaville', 'vila ol\xc3mpia', 'chacara', 'ch\xc9cara', 'morumbi', 'interlagos', 'berrini', 'faria lima', 'android', 'pmbok']
-    exclude_regexp = '|'.join(EXCLUDE_STR)
-    df = df[df['description'].str.contains(exclude_regexp, case=False) == False]
+    DESC_EXCLUDE_STR = ['alphaville', 'vila ol\xc3mpia', 'chacara', 'ch\xc9cara', 'morumbi', 'interlagos', 'berrini', 'faria lima', 'android', 'pmbok']
+    desc_exclude_regexp = '|'.join(DESC_EXCLUDE_STR)
+    df = df[df['description'].str.contains(desc_exclude_regexp, case=False) == False]
+    print df.shape
+
+    TITLE_EXCLUDE_STR = ['c#', 'junior', 'pleno', 'javascript', 'php', 'react', 'front end']
+    title_exclude_regexp = '|'.join(TITLE_EXCLUDE_STR)
+    df = df[df['title'].str.contains(title_exclude_regexp, case=False) == False]
     print df.shape
 
     df.to_sql("job_viewer_job", conn, if_exists="append", index=False)
